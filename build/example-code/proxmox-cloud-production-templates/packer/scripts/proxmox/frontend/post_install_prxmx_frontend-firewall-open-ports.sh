6fad56bfdb617eafdd38823d33435335691a1bb8
grace sopha <gdsopha24@gmail.com>
2023-02-28 16:19:49 -0600

    Remove used parts

--- build/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/frontend/post_install_prxmx_frontend-firewall-open-ports.sh
+++ build/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/frontend/post_install_prxmx_frontend-firewall-open-ports.sh
@@ -4,6 +4,4 @@ sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
 sudo firewall-cmd --zone=public --add-port=443/tcp --permanent
 sudo firewall-cmd --zone=public --add-port=3000/tcp --permanent
 
-sudo systemctl restart nginx
-
 sudo firewall-cmd --reload
