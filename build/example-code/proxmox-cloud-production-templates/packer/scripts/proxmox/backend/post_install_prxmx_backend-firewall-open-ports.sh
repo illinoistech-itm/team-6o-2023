017b5cfb21eaea33ac482f4a9b67ebf056161179
grace sopha <gdsopha24@gmail.com>
2023-02-28 16:32:55 -0600

    Enable and start firewall

--- build/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/backend/post_install_prxmx_backend-firewall-open-ports.sh
+++ build/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/backend/post_install_prxmx_backend-firewall-open-ports.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+# enable and start firewall
+sudo systemctl enable firewalld
+sudo systemctl start firewalld
 
+# port for mariadb
 sudo firewall-cmd --zone=meta-network --add-port=3306/tcp --permanent
 
 sudo firewall-cmd --reload
--- build/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/frontend/post_install_prxmx_frontend-firewall-open-ports.sh
+++ build/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/frontend/post_install_prxmx_frontend-firewall-open-ports.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# enable and start firewall
+sudo systemctl enable firewalld
+sudo systemctl start firewalld
 
 sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
 sudo firewall-cmd --zone=public --add-port=443/tcp --permanent
