4e6ffda3bd15257c09231e3f963f01bc83cc35b7
Aleks <akamenovic@hawk.iit.edu>
2023-02-28 15:39:09 -0600

    Add code to find client_id from file

--- code/Test_Build/routes/index.js
+++ code/Test_Build/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const data = require('../data/credentials.json');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hawkstagram' });
+  res.render('index', { title: 'Hawkstagram', client_id: data.web.client_id });
 });
 
 module.exports = router;
