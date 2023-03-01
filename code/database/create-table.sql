36a1ef6cd37d8ae1f3e9f8cad09f54b692e53ef0
grace sopha <gdsopha24@gmail.com>
2023-03-01 00:17:17 -0600

    Update create table sql injection

--- code/database/create-table.sql
+++ code/database/create-table.sql
@@ -1,17 +1,15 @@
-USE team6o;
-
 CREATE TABLE IF NOT EXISTS accounts 
 (
-uid int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-name varchar(50) NOT NULL,
-email varchar(100) NOT NULL,
-password varchar(100) NOT NULL
+  uid int(11) AUTO_INCREMENT PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  email varchar(100) NOT NULL,
+  password varchar(100) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS images 
 (
-uid int(11) NOT NULL FOREIGN KEY,
-imageid NOT NULL AUTO_INCREMENT PRIMARY KEY,
-imagesrc TEXT
-caption varchar(180),
+  imageid int(11) AUTO_INCREMENT PRIMARY KEY,
+  imagesrc TEXT NOT NULL,
+  caption varchar(180),
+  uid int(11) FOREIGN KEY REFERENCES accounts(uid)
 );
