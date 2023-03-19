6f8b2b303d686d3a161a996c0b2d0d25985bba9c
Aleksandar Kamenovic <akamenovic@hawk.iit.edu>
2023-03-19 04:05:36 -0500

    Have loadbalancer download repo

--- build/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/loadbalancer/move-nginx-files.sh
+++ build/example-code/proxmox-cloud-production-templates/packer/scripts/proxmox/loadbalancer/move-nginx-files.sh
@@ -5,6 +5,9 @@
 # to allow the load balancing to take place
 # Change team00 to your team repo
 
+cd /home/vagrant
+su - vagrant -c "git clone git@github.com:illinoistech-itm/team-6o-2023.git"
+
 # This overrides the default nginx conf file enabling loadbalacning and 443 TLS only
 sudo cp -v /home/vagrant/team-6o-2023/code/nginx/nginx.conf /etc/nginx/
 sudo cp -v /home/vagrant/team-6o-2023/code/nginx/default /etc/nginx/sites-available/
