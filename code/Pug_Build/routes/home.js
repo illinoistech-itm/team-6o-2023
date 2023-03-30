13bd3c67a8455cf0b654e38056f06ca01dfb1806
Andrew Haines <hainesa5275@gmail.com>
2023-03-30 04:27:03 -0500

    Finished post creation process.

--- code/Pug_Build/routes/home.js
+++ code/Pug_Build/routes/home.js
@@ -35,12 +35,20 @@ router.post('/', function(req, res, next) {
   console.log("Email: " + responsePayload.email);
   */
   signUp(responsePayload)
-
+  global.email = responsePayload.email;
   res.redirect('/home/' + responsePayload.sub);
 });
-
-router.get('/:id', postController.showPosts);
-router.post('/:id', postController.createPostPost);
+router.get('/:id', async function(req, res, next){
+  const allPosts = await findAllPosts();
+  console.log(allPosts);
+  res.render('home_logged_in',{title: 'Posts', posts: allPosts})
+})
+router.post('/:id', async function(req, res, next){
+  console.log(req);
+  const newPost = new Post('', req.body.caption, Date(), global.email);
+  await createPost(newPost);
+  res.redirect(`/home/feed`)
+});
 
 async function signUp(responsePayload) {
   try {
@@ -62,4 +70,29 @@ async function signUp(responsePayload) {
   if (conn) return conn.end();
 }
 }
+
+const Post = require('../Posts/post')
+
+
+async function findAllPosts(){
+    const statement = await pool.query("SELECT * FROM posts");
+    //console.log(statement);
+    const rows = statement;
+    let posts = [];
+    rows.forEach((row) => {
+        const post = new Post(row.pid, row.caption, row.date, row.email);
+        posts.push(post);
+    });
+    //console.log(posts);
+    return posts;
+}
+
+async function createPost (posts) {
+    conn = await pool.getConnection();
+    const statement = await conn.prepare("INSERT INTO posts (caption, date, email) VALUES (?, ?, ?)");
+    const createdPost = await statement.execute([posts.caption, posts.date, posts.email]);
+    //console.log(createdPost);
+   // console.log(`Post with ID ${createdPost.lastInsertRowid} has been created`);
+}
+
 module.exports = router;
