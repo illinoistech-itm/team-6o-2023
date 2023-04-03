572eb56f9290dfe710e5c15b2d750a3ad3bd7026
Aleksandar Kamenovic <akamenovic@hawk.iit.edu>
2023-04-03 05:08:29 -0500

    Clean up unneeded code

--- code/Pug_Build/Posts/postController.js
+++ code/Pug_Build/Posts/postController.js
@@ -29,29 +29,4 @@ const postTableOperations = {
     
 }
 
-/*
-exports.showPosts = async function(req, res, next){
-    const allPosts = await findAllPosts();
-    console.log(allPosts);
-    res.render('home_logged_in',{title: 'Posts', posts: allPosts})
-}
-
-exports.createPostPost = async function(req, res, next){
-    console.log(req);
-    console.log(res);
-    const newPost = new Post('', req.body.caption, Date(), "email");
-    await createPost(newPost);
-    res.redirect(`/home/profile`)
-}
-
-
-async function createPost (posts) {
-    conn = await pool.getConnection();
-    const statement = await conn.prepare("INSERT INTO posts (caption, date, email) VALUES (?, ?, ?)");
-    const createdPost = await statement.execute([posts.caption, posts.date, posts.email]);
-    //console.log(createdPost);
-   // console.log(`Post with ID ${createdPost.lastInsertRowid} has been created`);
-}
-*/
-
 module.exports = postTableOperations;
