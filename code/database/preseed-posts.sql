ab4868c980a0b46f5513cf6f7d3a91acfa3b48fa
Aleksandar Kamenovic <akamenovic@hawk.iit.edu>
2023-04-13 19:57:19 -0500

    Add one more example to preseed-posts

--- code/database/preseed-posts.sql
+++ code/database/preseed-posts.sql
@@ -1,1 +1,2 @@
 INSERT INTO `itmt430`.`posts` (`caption`, `uid`, `created`, `image`) VALUES ('Text', 'uid', 'Fri, 14 Apr 2023 00:34:12 GMT', 'imageID');
+INSERT INTO `itmt430`.`posts` (`caption`, `uid`, `created`) VALUES ('Test', '2', 'Fri, 14 Apr 2023 00:34:12 GMT');
